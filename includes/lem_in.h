b515ae30549b6e1b1e6431b7a0426c32ec4957f3
lzhansh <zhanshayeva@gmail.com>
2020-04-25 13:15:31 -0700

    update

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem_in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/25 12:24:53 by marvin            #+#    #+#             */
+/*   Updated: 2020/04/25 12:24:53 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LEM_IN_H
 #define LEM_IN_H
 
@@ -6,23 +18,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-// #include ""
 
-#define	ANT_ERROR	"Error: Number of ants should be positive int"
-#define	GNL_ERROR	"Error: Cannot read the file"
-#define	INIT_ERROR	"Error: Initialization problem"
-#define	ROOM_ERROR	"Error: Invalid room"
-#define	ROOM2_ERROR	"Error: No mandatory comments"
-#define	LINK_ERROR	"Error: Invalid link"
-#define PATH_ERROR	"Error: There is no valid solution"
-#define	MOVE_ERROR	"Error: Cannot move the ants"
+# define			ANT_ERROR	"Error: Number of ants should be positive int"
+# define			GNL_ERROR	"Error: Cannot read the file"
+# define			INIT_ERROR	"Error: Initialization problem"
+# define			ROOM_ERROR	"Error: Invalid room"
+# define			ROOM2_ERROR	"Error: No mandatory comments"
+# define			LINK_ERROR	"Error: Invalid link"
+# define			PATH_ERROR	"Error: There is no valid solution"
+# define			MOVE_ERROR	"Error: Cannot move the ants"
 
 typedef enum
 {
 	START,
 	MIDDLE,
 	END
-}	t_type;
+}					t_type;
 
 typedef struct		s_room
 {
@@ -89,54 +100,49 @@ typedef struct		s_lemin
 
 }					t_lemin;
 
-int		parse_ants(char **f);
-void	parse_rooms(t_lemin *lemin, char *str, char **f);
-void	parse_link(t_lemin *lemin, char *str, char **f);
+int					parse_ants(char **f);
+void				parse_rooms(t_lemin *lemin, char *str, char **f);
+void				parse_link(t_lemin *lemin, char *str, char **f);
 
-t_room	*create_room(char *str, t_type type);
-void	add_room(t_lemin *lemin, t_room *room);
-void	check_room(t_lemin *lemin, t_room *room);
+t_room				*create_room(char *str, t_type type);
+void				add_room(t_lemin *lemin, t_room *room);
+void				check_room(t_lemin *lemin, t_room *room);
 
-t_link	*create_link(t_lemin *lemin, char *str);
-void	add_link(t_lemin *lemin, t_link *link);
+t_link				*create_link(t_lemin *lemin, char *str);
+void				add_link(t_lemin *lemin, t_link *link);
 
-void	free_strsplit(char ***strsplit);
-void	error(char *s);
-bool	is_comment(char *str);
-bool	is_valid_comment(char *str);
-bool	is_room(char *str);
-bool	is_link(t_lemin *lemin, char *str);
-t_room	*room_name(t_lemin *lemin, char *str);
+void				free_strsplit(char ***strsplit);
+void				error(char *s);
+bool				is_comment(char *str);
+bool				is_valid_comment(char *str);
+bool				is_room(char *str);
+bool				is_link(t_lemin *lemin, char *str);
+t_room				*room_name(t_lemin *lemin, char *str);
+			
+void				create_routes(t_lemin *lemin);
+			
+t_link				*create_link(t_lemin *lemin, char *str);
+void				add_link(t_lemin *lemin, t_link *link);
+void				check_links(t_lemin* lemin);
+			
+void				move_ants(t_lemin *lemin);
+			
+void				free_positions(t_pos **pos);
+t_pos				*new_position(int ant, t_room *room);
+void				add_position(t_lemin *lemin, t_pos *pos);
 
-void	create_routes(t_lemin *lemin);
+void				print_ans(t_lemin *lem);
 
-t_link		*create_link(t_lemin *lemin, char *str);
-void		add_link(t_lemin *lemin, t_link *link);
-void		check_links(t_lemin* lemin);
+void				remove_it(t_lemin *lemin, t_link *link);
+void				remove_link(t_lemin *lemin);
+void				remove_ends(t_lemin *lemin);
+void				remove_input(t_lemin *lemin);
+void				remove_output(t_lemin *lemin);
 
-void		move_ants(t_lemin *lemin);
+void				remove_o_link(t_lemin *lemin, t_room *room);
+void				remove_i_link(t_lemin *lemin, t_room *room);
+t_link				*find_link(t_lemin *lemin, t_room *s, t_room *e);
 
-void	free_positions(t_pos **pos);
-t_pos	*new_position(int ant, t_room *room);
-void	add_position(t_lemin *lemin, t_pos *pos);
-
-void	print_ans(t_lemin *lem);
-
-void	remove_it(t_lemin *lemin, t_link *link);
-void	remove_link(t_lemin *lemin);
-void	remove_ends(t_lemin *lemin);
-void	remove_input(t_lemin *lemin);
-void	remove_output(t_lemin *lemin);
-
-void		remove_o_link(t_lemin *lemin, t_room *room);
-void	remove_i_link(t_lemin *lemin, t_room *room);
-t_link	*find_link(t_lemin *lemin, t_room *s, t_room *e);
-
-void	set_levels(t_lemin *lemin);
-
+void				set_levels(t_lemin *lemin);
 
 #endif
-
-//Parse the input
-//Create paths using queue
-//
